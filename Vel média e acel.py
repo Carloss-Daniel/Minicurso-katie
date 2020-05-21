4fc2b0ced264436a68b9668e090e32c2cb38c517
EuDanzin1 <65622154+EuDanzin1@users.noreply.github.com>
2020-05-21 16:51:06 -0300

    Add files via upload

---
+++ Vel média e acel.py
@@ -0,0 +1,8 @@
+Espaço = float (input('Informe o Espaço (metros): '))
+Tempo = float(input('Informe o Tempo (segundos): '))
+Velocidade_Media = Espaço/Tempo
+
+print('Sua velocidade média é de:', Velocidade_Media,'m/s')
+
+aceleração = (Velocidade_Media/Tempo)
+print('Sua aceleração é de:', aceleração,'m/s²')
