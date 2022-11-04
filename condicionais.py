91a62647edb36aaaaee44d6f525a66aa026d4090
Carlos Daniel <65622154+Carloss-Daniel@users.noreply.github.com>
2022-11-04 19:45:50 -0300

    Update condicionais.py

--- condicionais.py
+++ condicionais.py
@@ -1,9 +1,15 @@
-a= 5
-b= 10
-if a > b:   print(a, 'é maior que',b)
-else:   print(b,'é maior que',a)
+a = 5
+b = 10
+if a > b:
+  print(a, 'é maior que',b)
+else:
+  print(b,'é maior que',a)
 
-nome1= 'carlos'
-nome2= 'daniel'
-if nome1==nome2:    print('iguais')
-else: print('diferentes')
+nome1 = 'carlos'
+nome2 = 'daniel'
+
+if nome1 == nome2:
+  print('iguais')
+  
+else:
+  print('diferentes')
